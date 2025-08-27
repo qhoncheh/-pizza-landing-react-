6ecc6bfec36415d1991d5f635395fd9f1e94eb21
qhoncheh <ghoncheh.ataee@gmail.com>
2025-08-27 12:40:07 +0330

    .

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,6 @@
 interface MessageProps {
     count: number;
-}
+} 
 const Message = (props: MessageProps) => {
   return (
       <p> you can see number og count <strong>{props.count}</strong> yes !</p>
