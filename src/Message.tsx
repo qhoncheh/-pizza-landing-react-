27e7f892e731df1cee252b08e8f39a40d90590ff
qhoncheh <ghoncheh.ataee@gmail.com>
2025-08-29 18:11:05 +0330

    impliment index

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-import type { MessageProps } from "./types"
-
-const Message = (props: MessageProps) => {
-  return (
-      <p> you can see number og count <strong>{props.count}</strong> yes !</p>
-  )
-}
-
-export default Message
