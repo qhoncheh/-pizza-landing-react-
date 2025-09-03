4191f05a9f901f5888e17261370b56a80249ddeb
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-03 15:31:32 +0330

    refactor

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,22 @@
-export type pizzaDataTypes = {
-  pic: string;
+export type pizzaDataTypes =  {
   name: string;
-  desc: string;
+  ingredients: string;
   price: number;
-};
+  photoName: string;
+  soldOut: boolean;
+}
 
 export type SectionTitleProps = {
   text: string;
-  textColor?: string;
-  borderColor?: string;
-  className?: string;
-};
+  textColor?: string;     
+  borderColor?: string;    
+  className?: string;     
+}
 
 export type PizzaProp = {
   pic: string;
   name: string;
   desc: string;
-  price: string;
+  price: number;
+  className: string
 };
