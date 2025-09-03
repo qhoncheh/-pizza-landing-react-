4b5f416cc000f2df3f58580ac0227ad5c19245d0
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-03 09:59:28 +0330

    refactor

--- src/components/custom/section-title.tsx
+++ src/components/custom/section-title.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
 import type { SectionTitleProps } from '../../types';
 
-const SectionTitle: React.FC<SectionTitleProps> = ({
-  text,
-  textColor = 'text-gray-400',
-  borderColor = 'border-gray-300',
-  className = '',
-}) => {
+const SectionTitle = ({ text, textColor, borderColor, className }: SectionTitleProps) => {
+  
   return (
     <div className={`flex items-center gap-4 my-8 ${className}`}>
-      <div className={`flex-grow border-t ${borderColor}`}></div>
-      <h2 className={`text-xl font-semibold ${textColor}`}>{text}</h2>
+      <div className={`flex-grow border-t border-gray-400 ${borderColor}`}></div>
+      <h2 className={`text-xl font-semibold text-gray-400 ${textColor}`}>{text}</h2>
       <div className={`flex-grow border-t ${borderColor}`}></div>
     </div>
   );
