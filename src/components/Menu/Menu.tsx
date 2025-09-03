396948c7114e421760a012df63ba612bac62f875
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-03 09:37:47 +0330

    install tailwindcss version 3

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,12 +1,10 @@
 import Pizza from "../../Pizza"
-import Header from "../Header/Header"
 
 const Menu = () => {
     return (
         <div>
             <h2>Our Menu</h2>
-            <Header />
-            <Pizza />
+            <Pizza /> 
             <Pizza />
             <Pizza />
       </div>
