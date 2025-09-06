b27cb9b8ae9784e6a014abea6bd6f9982a26b097
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-06 11:15:52 +0330

    fix type errors

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,25 @@
-export type pizzaDataTypes =  {
+export type pizzaDataTypes = {
   name: string;
   ingredients: string;
   price: number;
   photoName: string;
+  pic: string;
   soldOut: boolean;
-}
+  desc: string;
+  className?: string;
+};
 
 export type SectionTitleProps = {
   text: string;
-  textColor?: string;     
-  borderColor?: string;    
-  className?: string;     
-}
+  textColor?: string;
+  borderColor?: string;
+  className?: string;
+};
 
 export type PizzaProp = {
   pic: string;
   name: string;
   desc: string;
   price: number;
-  className: string 
-}
+  className?: string;
+};
