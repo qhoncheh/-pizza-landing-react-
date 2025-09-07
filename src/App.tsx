db6d2da9e682450e9ae8beb9ea4466534b4d61c8
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-07 13:08:12 +0330

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 
 export default function App() {
 
     return (
         <div className="bg-[#060606] w-full flex flex-col gap-2 justify-center min-h-screen">
-            <Header />
             <Menu />
             <Footer />
         </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 const Header = () => {
   return (
     <header>
-      <div className="border-2 h-32 border-gray">vvvvv</div>
       <h3 className="text-[#D2B57A] p-4 font-bold text-3xl text-center">🍕 Pizza Co.</h3>
     </header>
   )
