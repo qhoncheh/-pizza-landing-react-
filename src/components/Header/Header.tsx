0451afac038ca1161109d54b11b3cd5a69f0d193
qhoncheh <ghoncheh.ataee@gmail.com>
2025-09-07 09:07:34 +0330

    added icons for Header and index app

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
       <div className="border-2 h-32 border-gray">vvvvv</div>
-      <h3 className="text-[#D2B57A] p-4 font-bold text-3xl text-center"> Pizza Co.</h3>
+      <h3 className="text-[#D2B57A] p-4 font-bold text-3xl text-center">🍕 Pizza Co.</h3>
     </div>
   )
 }
